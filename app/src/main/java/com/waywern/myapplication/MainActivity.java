7a240d7c7a64ed5afa7f4b54ea1041b4e9a9d75e
MOA.MIGHTY\slisovtsov <waywern@gmail.com>
2016-09-12 14:12:02 -0400

    Play with logs. Create all method for cover lifecicle of app

--- app/src/main/java/com/waywern/myapplication/MainActivity.java
+++ app/src/main/java/com/waywern/myapplication/MainActivity.java
@@ -2,7 +2,8 @@ package com.waywern.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
     import android.os.Bundle;
-    import android.view.View;
+import android.util.Log;
+import android.view.View;
     import android.widget.Button;
     import android.widget.TextView;
     import android.widget.Toast;
@@ -21,66 +22,93 @@ import android.support.v7.app.AppCompatActivity;
                     new TrueFalse(R.string.question_asia, true),
             };
     private int mCurrentIndex = 0;
+    private String TAG = "MainActivity";
 
             @Override
     protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+        setContentView(R.layout.activity_main);
 
-                    mTrueButton = (Button)findViewById(R.id.true_button);
+        mTrueButton = (Button)findViewById(R.id.true_button);
 
-                    mTrueButton.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                    checkAnswer(true);
-                                }
-                        });
-
-                    mFalseButton = (Button)findViewById(R.id.false_button);
-            mFalseButton.setOnClickListener( new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                            checkAnswer(false);
-                        }
-                });
-
-                    mQuestionTextView = (TextView)findViewById(R.id.question_textView);
-            updatequestion();
-
-                    mNextButton = (Button)findViewById(R.id.next_button);
-            mNextButton.setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v){
-                            mCurrentIndex = (mCurrentIndex  + 1) % mQuestionBank.length;
-                            updatequestion();
-                        }
-                });
-            mPreviouseButton  = (Button)findViewById(R.id.previouse_button);
-            mPreviouseButton.setOnClickListener(new View.OnClickListener(){
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v){
-                    if(mCurrentIndex == 0)
-                        mCurrentIndex = mQuestionBank.length-1;
-                    else
-                        mCurrentIndex = mCurrentIndex  - 1;
-                    updatequestion();
-                }
+                public void onClick(View v) {
+                        checkAnswer(true);
+                    }
             });
 
-        }
-    public void updatequestion()
-    {
-            int question = mQuestionBank[mCurrentIndex].getQuestion();
-            mQuestionTextView.setText(question);
-        }
+        mFalseButton = (Button)findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener( new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                        checkAnswer(false);
+                    }
+            });
 
-            public void checkAnswer(boolean answer)
+        mQuestionTextView = (TextView)findViewById(R.id.question_textView);
+        updatequestion();
+
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                        mCurrentIndex = (mCurrentIndex  + 1) % mQuestionBank.length;
+                        updatequestion();
+                    }
+            });
+        mPreviouseButton  = (Button)findViewById(R.id.previouse_button);
+        mPreviouseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(mCurrentIndex == 0)
+                    mCurrentIndex = mQuestionBank.length-1;
+                else
+                    mCurrentIndex = mCurrentIndex  - 1;
+                updatequestion();
+            }
+        });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    public void updatequestion() {
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+    }
+    public void checkAnswer(boolean answer)
     {
-            int pointer_to_text;
-            if(answer == mQuestionBank[mCurrentIndex].isAnswer())
-                    pointer_to_text = R.string.correct_toast;
-            else
-                pointer_to_text = R.string.incorrect_toast;
-            Toast.makeText(MainActivity.this, pointer_to_text, Toast.LENGTH_SHORT).show();
-        }
+        int pointer_to_text;
+        if(answer == mQuestionBank[mCurrentIndex].isAnswer())
+                pointer_to_text = R.string.correct_toast;
+        else
+            pointer_to_text = R.string.incorrect_toast;
+        Toast.makeText(MainActivity.this, pointer_to_text, Toast.LENGTH_SHORT).show();
+    }
 }
